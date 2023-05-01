a28307ed16ce77b038af5e768afa1d96287cd20e
Jonas Pedersen <jpe@kvalitetsit.dk>
2023-05-01 14:06:37 +0200

    Prepare for central maven deployment.

--- build/maven-run.sh
+++ build/maven-run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 # Build inside docker container
-docker run -e GITHUB_TOKEN=$GITHUB_TOKEN -v /var/run/docker.sock:/var/run/docker.sock -v $(pwd):/src -v $HOME/.m2:/root/.m2 maven:3-jdk-11 /src/build/maven-deploy.sh
+docker run -e GITHUB_TOKEN=$GITHUB_TOKEN -e MAVEN_USERNAME=$MAVEN_USERNAME -e MAVEN_PASSWORD=$MAVEN_PASSWORD -e SIGN_KEY_PASS=$SIGN_KEY_PASS -e SIGN_KEY_ID=$SIGN_KEY_ID -e SIGN_KEY="$SIGN_KEY" -v /var/run/docker.sock:/var/run/docker.sock -v $(pwd):/src -v $HOME/.m2:/root/.m2 maven:3-jdk-11 /src/build/maven-deploy.sh
