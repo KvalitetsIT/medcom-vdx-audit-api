17e64ff9b5b50ad950e3bebc77a5ddb8924db11c
Jonas <jqh@kvalitetsit.dk>
2026-06-25 09:07:38 +0200

    added another missing javaTimeModule

--- src/main/java/dk/kvalitetsit/audit/client/messaging/nats/NatsPublisher.java
+++ src/main/java/dk/kvalitetsit/audit/client/messaging/nats/NatsPublisher.java
@@ -2,6 +2,7 @@ package dk.kvalitetsit.audit.client.messaging.nats;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import dk.kvalitetsit.audit.client.messaging.MessagePublisher;
 import io.nats.client.JetStream;
 import io.nats.client.JetStreamApiException;
@@ -24,6 +25,7 @@ public class NatsPublisher implements MessagePublisher {
 
     public void publishMessage(Object message) throws JetStreamApiException, IOException {
         ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
         mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
         mapper.disable(SerializationFeature.WRITE_DATE_TIMESTAMPS_AS_NANOSECONDS);
         String jsonResult = mapper.writeValueAsString(message);
